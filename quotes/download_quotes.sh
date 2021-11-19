bb4eedaf43e45b6e7a001402e8bc2675f7db7ce3
Zeeshan Khan <zkhan1093@gmail.com>
2021-11-19 21:24:17 +0530

    fix path of downloaded quote files

--- quotes/download_quotes.sh
+++ quotes/download_quotes.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-wget -o ~/.conky/quotes/quotes.log -O ~/.conky/quotes/quotes.json https://gist.githubusercontent.com/zkhan93/994a6dc9718c3a3f0c6d97dcdae0e81c/raw
-/usr/bin/python3 ~/.conky/quotes/select_quote.py
+wget -o ~/.conky/quotes/data/quotes.log -O ~/.conky/quotes/quotes.json https://gist.githubusercontent.com/zkhan93/994a6dc9718c3a3f0c6d97dcdae0e81c/raw
+/usr/bin/python3 ~/.conky/quotes/data/select_quote.py
